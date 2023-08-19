057b87282205b9e9e9c2f671e6021f9322e2d5eb
FrancisTR <123771828+FrancisTR@users.noreply.github.com>
2023-08-19 12:50:40 -0500

    Add files via upload

---
+++ src/components/ListDetails.js
@@ -0,0 +1,34 @@
+const ListDetails = ({ result }) => {
+  console.log({ result });
+
+  const { word, phonetics, meanings } = result;
+
+  return (
+    <div>
+      <div>
+        <h3>Word</h3>
+        <div>
+          <p>{word}</p>
+          <p>{meanings[0].partOfSpeech}</p>
+          <p>{phonetics[0].text}</p>
+        </div>
+
+        <div>
+          <h3>Meaning:</h3>
+          <p>{meanings[0].definitions[0].definition}</p>
+        </div>
+
+        <div>
+          <h3>Synonyms:</h3>
+          <p>
+            {meanings[0].synonyms.map((item, index) => {
+              return <p key={index}>{item}</p>;
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListDetails;
